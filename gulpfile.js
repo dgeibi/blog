86cb52423d418abe1d997e674148eacfdf494600
dgeibi <png.inside@gmail.com>
2017-06-21 16:42:51 +0800

    watch webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,17 @@
 const gulp = require('gulp');
 const liveServer = require('live-server');
+const path = require('path');
 
 const { shell, shellSync } = require('./utils/shell');
 
-gulp.task('default', [
-  'hexo:watch',
-  'serve',
-]);
+gulp.task('default', ['serve']);
 
-gulp.task('hexo:watch', () => {
-  shell('hexo generate --watch');
-});
-
-gulp.task('serve', ['hexo:watch'], () => {
-  const params = {
+gulp.task('serve', ['theme:watch', 'hexo:watch'], () => {
+  liveServer.start({
     port: 4000,
     root: 'public',
     logLevel: 2,
-  };
-  liveServer.start(params);
+  });
 });
 
 gulp.task('generate', () => {
@@ -31,4 +24,12 @@ gulp.task('deploy', ['generate'], (callback) => {
 
 gulp.task('push', () => {
   shellSync('git add -A && git commit -m "update at `date`" && git push');
+});
+
+gulp.task('theme:watch', () => {
+  shell(`cd themes/plain && npm run watch -- --env.dir=${path.resolve(__dirname, 'public/js')}`);
+});
+
+gulp.task('hexo:watch', () => {
+  shell('hexo g --watch');
 });
