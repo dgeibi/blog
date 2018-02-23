b8de7b529b05493b4357ac15912984ecd9968fbc
dgeibi <png.inside@gmail.com>
2018-02-23 19:25:31 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,77 @@
-const gulp = require('gulp');
-const liveServer = require('live-server');
-const path = require('path');
+const gulp = require('gulp')
+const liveServer = require('live-server')
+const path = require('path')
 
-const { shell, shellSync } = require('./utils/shell');
+const { shellSync, run } = require('./utils/shell')
 
-gulp.task('default', ['serve']);
+const errorlogger = error => {
+  console.log(error)
+}
 
-gulp.task('serve', ['theme:watch', 'hexo:watch'], () => {
-  liveServer.start({
-    port: 4000,
-    root: 'public',
-    logLevel: 2,
-  });
-});
+const buildHexo = () => run('hexo g')
 
-gulp.task('generate', () => {
-  shellSync('hexo clean && hexo g');
-});
+const buildTheme = () =>
+  run(`npm run -s build -- --env.dir=${path.resolve(__dirname, 'public/js')}`, {
+    cwd: path.join(__dirname, 'themes/plain'),
+  })
 
-gulp.task('deploy', ['generate'], (callback) => {
-  shell('firebase deploy', callback);
-});
+const watch = () =>
+  buildHexo().then(() => {
+    const watcher = gulp.watch([
+      `source/**/*`,
+      `themes/plain/layout/**/*`,
+      `themes/plain/src/**/*`,
+      `**/_config.yml`,
+      `themes/plain/**/.babelrc`,
+    ])
+    const handler = p => {
+      console.log(`${p} changed`)
+      if (
+        matchPath(p, ['source', 'themes/plain/layout']) ||
+        p.endsWith('_config.yml')
+      ) {
+        buildHexo().catch(errorlogger)
+      } else if (matchPath(p, 'themes/plain/src') || p.endsWith('.babelrc')) {
+        buildTheme().catch(errorlogger)
+      } else {
+        console.log('but nothing to do')
+      }
+    }
+    watcher.on('change', handler)
+    watcher.on('add', handler)
+  })
 
-gulp.task('push', () => {
-  shellSync('git add -A && git commit -m "update at `date`" && git push');
-});
+const serve = () =>
+  watch().then(() => {
+    liveServer.start({
+      port: 4000,
+      root: 'public',
+      logLevel: 2,
+    })
+  })
 
-gulp.task('theme:watch', () => {
-  shell(`cd themes/plain && npm run watch -- --env.dir=${path.resolve(__dirname, 'public/js')}`);
-});
+const generate = async () => {
+  await run('hexo clean')
+  await run('hexo g')
+}
 
-gulp.task('hexo:watch', () => {
-  shell('hexo g --watch');
-});
+const deploy = () => run('firebase deploy')
+
+const push = () => {
+  shellSync('git add -A && git commit -m "update at `date`" && git push')
+}
+
+gulp.task('serve', serve)
+gulp.task('generate', generate)
+gulp.task('deploy', gulp.series(generate, deploy))
+gulp.task('push', push)
+
+function matchPath(a, b) {
+  if (typeof b === 'string')
+    return path.normalize(a).startsWith(path.normalize(b))
+  else if (Array.isArray(b)) {
+    const na = path.normalize(a)
+    return b.some(x => na.startsWith(path.normalize(x)))
+  }
+  return false
+}
--- utils/shell.js
+++ utils/shell.js
@@ -1,14 +1,37 @@
-const { exec, execSync } = require('child_process');
+const { exec, execSync } = require('child_process')
+const spawn = require('cross-spawn')
 
 exports.shell = function shell(cmd = '', callback) {
-  const proc = exec(cmd, (error) => {
-    if (typeof callback === 'function') callback(error);
-  });
-  proc.stderr.pipe(process.stderr);
-  proc.stdout.pipe(process.stdout);
-  return proc;
-};
+  const proc = exec(cmd, error => {
+    if (typeof callback === 'function') callback(error)
+  })
+  proc.stderr.pipe(process.stderr)
+  proc.stdout.pipe(process.stdout)
+  return proc
+}
 
 exports.shellSync = function shellSync(cmd = '') {
-  execSync(cmd, { stdio: 'inherit' });
-};
+  execSync(cmd, { stdio: 'inherit' })
+}
+
+exports.run = function run(cmd, opts) {
+  cmd = cmd.trim()
+  const [command, ...args] = cmd.split(/\s+/)
+  const cp = spawn(command, args, { stdio: 'inherit', ...opts })
+  let done = false
+  return new Promise((resolve, reject) => {
+    cp.once('error', error => {
+      if (done) return
+      reject(error)
+      done = true
+    })
+    cp.once('close', code => {
+      if (done) return
+      if (code === 0) resolve()
+      else {
+        reject(Error(`"${cmd}" exit code is ${code}`))
+      }
+      done = true
+    })
+  })
+}
